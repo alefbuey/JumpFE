e5d391b85c93e9261bd03332222a855f80c42f65
Esteban Lasso <estebannl28@gmail.com>
2018-11-11 18:33:53 -0500

    Changes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -13,20 +13,10 @@ export const Tab1Root = 'NotificationPage';
 export const Tab2Root = 'ListMasterPage';
 export const Tab3Root = 'FavoritesPage';
 export const Tab4Root = 'ChatPage';
-
-<<<<<<< HEAD
-
-export const Myjobs = 'TabsJobPage';
-export const CurrentJob = 'CurrentJobPage';
-export const ApplyingJob = 'ApplyingJobPage';
-export const HistoryJob = 'HistoryJobPage';
-
-export const ProfilePage = 'ProfilePage';
-=======
 export const ProfilePage = 'ProfilePage';
 
 //MY JOBS
 export const MyjobsPage = 'MyjobsPage'; //It is a tab page
 export const ApplyingJobsPage = 'ApplyingJobsPage';
 export const AcceptedJobsPage = 'AcceptedJobsPage';
->>>>>>> 64a37f88d1870bf29c479eebce3a034cb0e0a205
+
